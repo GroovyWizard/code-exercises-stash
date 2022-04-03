861ec22ea2a554346fddfa3097bbf7ba85b43c36
GroovyWizard <tardyfox@gmail.com>
2022-04-03 18:50:59 -0300

    Added kotlin exercise about counting positive numbers of an Array and Sum the negative ones

--- kotlin/CountPositivesSumNegatives.kt
+++ kotlin/CountPositivesSumNegatives.kt
@@ -16,3 +16,7 @@ fun countPositivesSumNegatives(input : Array<Int>?) : Array<Int> {
 
     return result
 }
+
+//Given an array of integers.
+//Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+//If the input is an empty array or is null, return an empty array.
